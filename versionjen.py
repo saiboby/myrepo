102eb32debb3e96540bb3563e0e804c7f1dc250c
saiboby <saibabu.tan@gmail.com>
2022-07-08 19:14:58 +0530

    Update versionjen.py

--- versionjen.py
+++ versionjen.py
@@ -1,7 +1,5 @@
-import python-jenkins
-print jenkins.__file__
-
-server = python-jenkins.Jenkins('http://18.117.236.74:8080', username='admin', password='admin')
+import jenkins
+server = jenkins.Jenkins('http://18.117.236.74:8080', username='admin', password='admin')
 user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
