eb873326a5a21f57d229e4d0047741b4937348f2
saiboby <saibabu.tan@gmail.com>
2022-07-08 18:47:49 +0530

    Create jenkins.py

---
+++ jenkins.py
@@ -0,0 +1,6 @@
+import jenkins
+
+server = jenkins.Jenkins('http://localhost:8080', username='myuser', password='mypassword')
+user = server.get_whoami()
+version = server.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
