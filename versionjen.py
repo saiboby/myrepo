5e6a2d7ccb37a9f8a4e947aa2bb7f2d77a2cf61a
saiboby <saibabu.tan@gmail.com>
2022-07-08 19:18:51 +0530

    Update versionjen.py

--- versionjen.py
+++ versionjen.py
@@ -3,3 +3,25 @@ server = jenkins.Jenkins('http://18.117.236.74:8080', username='admin', password
 user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
+server.create_node('slave1')
+nodes = get_nodes()
+print nodes
+node_config = server.get_node_info('slave1')
+print node_config
+
+# create node with parameters
+params = {
+    'port': '22',
+    'username': 'ec2-user',
+    'credentialsId': 'hapro',
+    'host': 'newlb-2f94b85783160e54.elb.us-east-2.amazonaws.com'
+}
+server.create_node(
+    'slave1',
+    nodeDescription='my test slave',
+    remoteFS='/home/ec2-user/jenkins_home',
+    labels='precise',
+    exclusive=True,
+    launcher=jenkins.LAUNCHER_SSH,
+    launcher_params=params)
