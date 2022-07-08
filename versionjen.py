38c411c3268748d789f128d3784e1564111750f6
saiboby <saibabu.tan@gmail.com>
2022-07-08 20:28:05 +0530

    Update versionjen.py

--- versionjen.py
+++ versionjen.py
@@ -8,7 +8,12 @@ print('Hello %s from Jenkins %s' % (user['fullName'], version))
 nodes = server.get_nodes()
 print(nodes)
 
-
+params = {
+    'port': '22',
+    'username': 'juser',
+    'credentialsId': '10f3a3c8-be35-327e-b60b-a3e5edb0e45f',
+    'host': 'my.jenkins.slave1'
+}
 server.create_node(
     'slave1',
     nodeDescription='my test slave',
