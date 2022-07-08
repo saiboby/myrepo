8110377e91f8e02e65d51669c10a6c2b9a5ba971
saiboby <saibabu.tan@gmail.com>
2022-07-08 18:56:28 +0530

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -1,6 +1,6 @@
 import jenkins
 
-server = jenkins.Jenkins('http://18.117.236.74:8080', username='admin', password='admin')
+server = jenkins.jenkins('http://18.117.236.74:8080', username='admin', password='admin')
 user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
