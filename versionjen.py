3288ae5639c31ba379c2a74d14199b049e183366
saiboby <saibabu.tan@gmail.com>
2022-07-11 19:23:36 +0530

    Update versionjen.py

--- versionjen.py
+++ versionjen.py
@@ -8,7 +8,7 @@ print('Hello %s from Jenkins %s' % (user['fullName'], version))
 nodes = server.get_nodes()
 print(nodes)
 
-server.delete_node('slave22')
+#server.delete_node('slave22')
 
 params = {
     'port': '22',
